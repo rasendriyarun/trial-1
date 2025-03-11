da5a38fda5ebb1519936ed185b37188e03258bb2
rasendriyarun <rasendriyaarun@gmail.com>
2025-03-11 20:51:31 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import streamlit as st, pandas as pd, numpy as np
 
 st.title('Hello World')
-st.header('Play the world is ugly by my chemical romance')
+st.header('Play the world is ugly by my chemical romance please')
